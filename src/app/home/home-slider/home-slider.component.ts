60be5857d43283c65687a8d07b7e5cdaf6f6da6e
Ivonne Sanchez <ivonnesoriano@gmail.com>
2021-01-07 08:50:51 -0600

    added the scroll down+fixed style

--- src/app/home/home-slider/home-slider.component.ts
+++ src/app/home/home-slider/home-slider.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { SwiperModule, SwiperConfigInterface } from 'ngx-swiper-wrapper';
 import { home } from '../../../assets/info/data';
-
+import $ from 'jquery';
 
 @Component({
   selector: 'app-home-slider',
@@ -11,20 +11,70 @@ import { home } from '../../../assets/info/data';
 export class HomeSliderComponent implements OnInit {
 
   data;
-  constructor() { 
+  constructor() {
     this.data = home;
   }
 
   ngOnInit(): void {
+    $(document).ready(function () {
+      var heroheight = $("#home").height();	
+      $(".scrollDown").on('click', function(){
+        $("html,body").animate({scrollTop: heroheight}, 800);
+      })
+    });
+
+
   }
   index;
   config: SwiperConfigInterface = {
+    resistance: true,
+    resistanceRatio: 0.1, speed: 1000,
+    autoplay: false,
+    mousewheel: true,
+    parallax: true,
     pagination: {
       el: '.pagination',
-      renderBullet: function (index, bullet) {
-        return '<span class="' + bullet + '"></span>';
+      clickable: true,
+      renderBullet: function (index, className) {
+        return '<span class="' + className + '">' + '<div class="parallax-wrap">' + '<div class="parallax-element"></div></div></span>';
       },
     },
-  };
+    on: {
+      slideChangeTransitionStart: function () {
 
+        $('.swiper-slide-active').find('div').each(function () {
+          if (!$(this).hasClass("active")) {
+            $(this).trigger('click');
+          }
+        });
+
+        $('.swiper-slide-duplicate-active').find('div').each(function () {
+          if (!$(this).hasClass("active")) {
+            $(this).trigger('click');
+          }
+        });
+
+        if ($('.swiper-slide-active').hasClass("change-header")) {
+          $('#page-content').removeClass('light-content');
+          $('#magic-cursor').removeClass('light-content');
+        } else {
+          $('#page-content').addClass('light-content');
+          $('#magic-cursor').addClass('light-content');
+        }
+
+        if ($('.swiper-slide-duplicate-active').hasClass("change-header")) {
+          $('#page-content').removeClass('light-content');
+          $('#magic-cursor').removeClass('light-content');
+        } else {
+          $('#page-content').addClass('light-content');
+          $('#magic-cursor').addClass('light-content');
+        }
+        $('.swiper-slide').find('.slide-title').each(function () {
+          $(this).removeClass('active-title');
+        });
+
+
+      }
+    },
+  };
 }
