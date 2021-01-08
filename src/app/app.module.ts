06ed85f22e0c1d49e4323e891c9875dbd9a48cef
Ivonne Sanchez <ivonnesoriano@gmail.com>
2021-01-07 20:37:24 -0600

    removed innecesary directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from "./shared/shared.module";
 import { AppComponent } from './app.component';
-import { ScrollSpyDirective } from './directives/scroll-spy.directive';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ScrollSpyDirective,
+    AppComponent
   ],
   imports: [
     BrowserModule,
